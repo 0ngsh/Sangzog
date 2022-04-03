5a142b92905931e5338f8f930792b14b54c68f06
0ngsh <angshumaanborah@gmail.com>
2022-04-03 12:20:15 +0530

    creating pojo class

---
+++ app/src/main/java/in/angsh/sangzog/ui/school/School.java
@@ -0,0 +1,13 @@
+package in.angsh.sangzog.ui.school;
+
+public class School {
+    private String udise_code;
+    private String school_name;
+    private String management;
+    private String category;
+    private String respondant_name;
+    private String respondant_number;
+    private String head_name;
+    private String head_number;
+
+}
